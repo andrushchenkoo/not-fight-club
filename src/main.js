bcef3934a5fbdf63bcd930581a619912701a60f5
Oleksandr Andrushchenko <aandrushchenko1402@gmail.com>
2025-08-14 11:50:42 +0300

    feat: add registration page

--- src/main.js
+++ src/main.js
@@ -1,2 +1,23 @@
-import './style.css'
+import "./style.css";
 
+const registrationPage = document.querySelector(".registration");
+const registrationButton = document.querySelector(".registration__button");
+const inputValue = document.querySelector(".registration__input");
+
+let characterName;
+
+function registersCharacterName() {
+  characterName = inputValue.value;
+  registrationPage.classList.add('registration--close');
+}
+
+function controlButtonaVailability() {
+  if (inputValue.value) {
+    registrationButton.disabled = false;
+  } else {
+    registrationButton.disabled = true;
+  }
+}
+
+inputValue.addEventListener("input", controlButtonaVailability);
+registrationButton.addEventListener("click", registersCharacterName);
